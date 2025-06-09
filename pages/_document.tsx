9fd9c157a738c7b323d472279713220326037b64
Amirul Irfan <amirul.irfan.biz@gmail.com>
2025-06-09 09:42:55 +0800

    fix issue on load

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,19 +8,19 @@ class MyDocument extends Document {
           <link rel="icon" href="/favicon.ico" />
           <meta
             name="description"
-            content="See pictures from Next.js Conf and the After Party."
+            content={process.env.NEXT_PUBLIC_CLOUDINARY_DETAILS}
           />
           <meta property="og:site_name" content="nextjsconf-pics.vercel.app" />
           <meta
             property="og:description"
-            content="See pictures from Next.js Conf and the After Party."
+            content={process.env.NEXT_PUBLIC_CLOUDINARY_DETAILS}
           />
-          <meta property="og:title" content="Next.js Conf 2022 Pictures" />
+          <meta property="og:title" content={process.env.NEXT_PUBLIC_CLOUDINARY_TITLE} />
           <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:title" content="Next.js Conf 2022 Pictures" />
+          <meta name="twitter:title" content={process.env.NEXT_PUBLIC_CLOUDINARY_TITLE} />
           <meta
             name="twitter:description"
-            content="See pictures from Next.js Conf and the After Party."
+            content={process.env.NEXT_PUBLIC_CLOUDINARY_DETAILS}
           />
         </Head>
         <body className="bg-black antialiased">
